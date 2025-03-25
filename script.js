89ec74ace647a2898a3fea7023b5f28bbbe243bf
Upayan <upayandutta204@gmail.com>
2025-03-26 01:23:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const nextBtn = document.querySelector('.next-btn');
+const video = document.querySelector('.hero-video');
+
+const movieList=['videos/hero-1.mp4','videos/hero-2.mp4','videos/hero-3.mp4','videos/hero-4.mp4'];
+
+let index=0;
+
+nextBtn.addEventListener('click',()=>{
+    console.log("hello")
+    index+=1;
+    video.src=movieList[index];
+    if(index==3)
+        index=-1;
+})
